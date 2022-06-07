1e7a0d6cc983637bb19cddcd3902842cfb14bd7d
unknown <paganochristians@gmail.com>
2022-06-07 07:18:26 -0500

    fix Images, adds .env

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Home />
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header =()=>{
         <header className='header' id='header'>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand nav-link-header" href="/">Home#</a>
+                <a className="navbar-brand nav-link-header" href="/MyPortfolio">Home#</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
